98c850171f8d58ac4c853337aaa9b7c607f8115e
unknown <panchalic3@gmail.com>
2021-05-17 10:45:14 +0530

    this is the updated on for Olivia

---
+++ ProjectUpdatesMay17Olivia/app/customer.ts
@@ -0,0 +1,11 @@
+export class Customer {
+    public cusAddress : string;
+    public cusEmail : string;
+    public cusMobile : string;
+    public cusPassword : string;
+    public custId: number;
+    public custName : string;
+    constructor(){
+ }
+
+}
